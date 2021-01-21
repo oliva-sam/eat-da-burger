76444132c8d3fbf69afe405368cd601f39c5e17a
Melody Monarch <oliva.sam.so@gmail.com>
2021-01-21 12:27:49 -0600

    sql table added and seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Avocado Bacon Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Black Bean Burger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheese Burger", FALSE);
