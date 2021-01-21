dea0b8a2ed412cf7f2d29c37ae88c2853121419f
Melody Monarch <oliva.sam.so@gmail.com>
2021-01-21 12:51:55 -0600

    middleware code added

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+// Parse POST requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Listener
 app.listen(PORT, function() {
