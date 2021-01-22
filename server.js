0ab391036ae4974a0d85d22bc84e7da8fced8719
Melody Monarch <oliva.sam.so@gmail.com>
2021-01-21 18:01:45 -0600

    made express connect to the public folder with static

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controllers")
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.use(express.static("public"));
 
 // Parse POST requests
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +15,8 @@ app.use(express.json());
 //
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(routes)
 
 // Listener
 app.listen(PORT, function() {
