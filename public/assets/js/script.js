c9171d6e1bfea43c27a4fea7f9fe12a3373f5fa4
Melody Monarch <oliva.sam.so@gmail.com>
2021-01-26 16:24:11 -0600

    front end js rough basis finished where user can now interact and send requests

---
+++ public/assets/js/script.js
@@ -0,0 +1,37 @@
+$(function() {
+    $("#burgerBtn").on("click", function(event) {
+        event.preventDefault();
+
+        let newBurger = {
+            burger_name: $("#userBurger").val().trim(),
+            devoured: 0
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST", 
+            data: newBurger
+        }). then(
+            function() {
+                console.log("New Burger Added...");
+                location.reload();
+            }
+        );
+    });
+
+    $(".devourBtn").on("click", function(event) {
+        let id = $(this).data("id");
+        let hasDevour= {
+            devoured: 1
+        };
+
+        $.ajax("api/burgers/" + id, {
+            type: "PUT",
+            data: hasDevour
+        }).then (
+            function() {
+                console.log(`You have ${hasDevour} devoured ${id}`);
+                location.reload();
+            }
+        );
+    });
+});
